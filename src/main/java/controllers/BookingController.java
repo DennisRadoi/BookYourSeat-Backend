ed6b21e71950f1f500937095558d6a70ef34b1df
Jorj19 <buneageorge29@gmail.com>
2026-08-19 11:37:51 +0300

    rezolvare buguri api

--- src/main/java/controllers/BookingController.java
+++ src/main/java/controllers/BookingController.java
@@ -1,11 +1,13 @@
 package controllers;
 
 import services.BookingService;
+import services.UserService;
 import dto.CreateBookingRequest;
 import dto.UpdateBookingRequest;
 import dto.UpdateRecurringBookingRequest;
 import dto.GetBookingResponse;
 import dto.GetRecurringBookingResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,18 +16,16 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 public class BookingController {
 
     private final BookingService bookingService;
-
-    public BookingController(BookingService bookingService) {
-        this.bookingService = bookingService;
-    }
+    private final UserService userService;
 
     @GetMapping("/bookings/me")
     public ResponseEntity<List<GetBookingResponse>> getMyBookings(
-            @RequestParam(name = "userId") Integer userId,
             @RequestParam(name = "status", required = false) String status) {
+        Integer userId = userService.getCurrentUser().getId();
         return ResponseEntity.ok(bookingService.getUserBookingsDTO(userId, status));
     }
 
--- src/main/java/dto/ColleagueResponse.java
+++ src/main/java/dto/ColleagueResponse.java
@@ -2,12 +2,13 @@ package dto;
 
 import entities.User;
 
-public record ColleagueResponse(String fullname, String role,
+public record ColleagueResponse(Integer id, String fullname, String role,
                                 String status, String location, boolean isFavorite) {
 
     public static ColleagueResponse fromEntity(User user, String status, String location,
                                                boolean isFavorite) {
         return new ColleagueResponse(
+                user.getId(),
                 user.getFirstName() + " " + user.getLastName(),
                 user.getRole(),
                 status,
